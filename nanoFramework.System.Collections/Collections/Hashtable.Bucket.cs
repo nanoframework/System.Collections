948f1e823cddb3eb0a6d52f18c4b543f2050263b
josesimoes <jose.simoes@eclo.solutions>
2022-03-04 19:37:27 +0000

    Rework HashTable implementation
    - Constructor is now aligned with .NET.
    - Remove unnecessary properties related with load factor.
    - Add parameter checks that were missing in most methods.
    - Improve several methods and constructors.
    - Add IDictionaryEnumerator to follow .NET.
    - Move hot path methods to native.
    - Improve comments for Intellisense.
    - Add more Unit Tests.

---
+++ nanoFramework.System.Collections/Collections/Hashtable.Bucket.cs
@@ -0,0 +1,25 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace System.Collections
+{
+    internal class Bucket
+    {
+        public object _key;
+        public object _value;
+        public uint _hash;
+
+        public Bucket(
+            object key,
+            object value,
+            uint hash)
+        {
+            _key = key;
+            _value = value;
+            _hash = hash;
+        }
+    }
+}
