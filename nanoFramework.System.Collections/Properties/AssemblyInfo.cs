a4328ce6c77b3fa8795129b4caa8b41a3d9323af
José Simões <jose.simoes@eclo.solutions>
2024-09-26 14:19:30 +0100

    Hashtable now uses correctly the GetHashCode() of the keys (#132)
    ***NO_CI***

--- nanoFramework.System.Collections/Properties/AssemblyInfo.cs
+++ nanoFramework.System.Collections/Properties/AssemblyInfo.cs
@@ -1,6 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -12,10 +16,6 @@ using System.Runtime.InteropServices;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.0.1.0")]
+[assembly: AssemblyNativeVersion("100.0.2.0")]
 ////////////////////////////////////////////////////////////////
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
