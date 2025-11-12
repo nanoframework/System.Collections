9fe03e33ab08ce0d2d2638db7ea49d35a4e5748f
José Simões <jose.simoes@eclo.solutions>
2025-11-12 17:43:39 +0000

    Add List<T> and related classes
    - Add unit tests.
    - Bump native assembly version.
    - Update nuspec

---
+++ nanoFramework.System.Collections/Collections/Generic/IReadOnlyCollection.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Represents a strongly-typed, read-only collection of elements.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the elements. This type parameter is covariant. That is, you can use either the type you specified or any type that is more derived.
+    /// </typeparam>
+    public interface IReadOnlyCollection<out T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Gets the number of elements in the collection.
+        /// </summary>
+        int Count
+        {
+            get;
+        }
+    }
+}
