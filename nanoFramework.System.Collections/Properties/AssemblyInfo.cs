1cb4ba6f587bc3d7ad589ae176945592598cf5c1
José Simões <jose.simoes@eclo.solutions>
2026-01-08 12:33:33 +0000

    Remove interfaces for generics collections
    - Update mscorlib reference.
    - Bump native assembly version.

--- nanoFramework.System.Collections/Properties/AssemblyInfo.cs
+++ nanoFramework.System.Collections/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Reflection;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.2.0.1")]
+[assembly: AssemblyNativeVersion("100.2.0.2")]
 ////////////////////////////////////////////////////////////////
